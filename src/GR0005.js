20a5d05a1d509a3438c9b6414ee03d825c415ba7
GuardRails Integration Tester <test@guardrails.io>
2018-07-16 12:30:17 +0700

    fix(buffer): fix the security issues detected by GuardRails [ci skip]

--- src/GR0005.js
+++ src/GR0005.js
@@ -1,9 +1,7 @@
-require('./current_file').file(__filename);
+require("./current_file").file(__filename);
 
 /*
 * [GR:0005:stable]
 */
 var a = Buffer.alloc(10);
-a.readUInt8(0, true)
-
-
+a.readUInt8(0);
